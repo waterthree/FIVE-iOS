1bb4601db58ce4c87162f017926399c80a42b5ff
Jianmiao Cai <waterthree@gmail.com>
2025-02-23 13:07:53 -0500

    init project on Xcode

---
+++ FIVE-iOS/FIVE_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIVE_iOSApp.swift
+//  FIVE-iOS
+//
+//  Created by Jianmiao Cai on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FIVE_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FIVE-iOSTests/FIVE_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FIVE_iOSTests.swift
+//  FIVE-iOSTests
+//
+//  Created by Jianmiao Cai on 2/23/25.
+//
+
+import Testing
+@testable import FIVE_iOS
+
+struct FIVE_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
